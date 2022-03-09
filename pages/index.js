26c54d48c88d1a22a52acbcf27d35420d9902d6d
Fabio Rodriguez <alessf13@gmail.com>
2022-03-09 10:24:01 +0000

    Loader and Nav components created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Loader from '../Components/Loader'
 
 export default function Home() {
   return (
-  <></>
+  <>
+    <Loader show /> 
+  </>
   )
 }
