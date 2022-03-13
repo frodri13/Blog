e2368f2f7078f54b6be40b0c75603af08fdd901c
Fabio Rodriguez <alessf13@gmail.com>
2022-03-13 15:10:00 +0000

    PostFeed component created

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,10 @@
+import UserProfile from "../../Components/UserProfile"
+import PostFeed from "../../Components/PostFeed"
+
 export default function UserProfilePage({user, posts}){
     return(
         <main>
-            
+
         </main>
     )
 }
