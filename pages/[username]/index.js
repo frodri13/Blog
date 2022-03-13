2c75a3670bf557c67751fd82201d8e8180eb433c
Fabio Rodriguez <alessf13@gmail.com>
2022-03-13 15:34:04 +0000

    getUserWithUsername helper function added to firebase.js

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,6 +1,23 @@
 import UserProfile from "../../Components/UserProfile"
 import PostFeed from "../../Components/PostFeed"
 
+export async function getServerSideProps({query: urlQuery}){
+    const {username} = urlQuery;
+
+    const userDoc = await getUserWithUsername(username);
+
+    // if no user, short circuit to 404 page
+    // if (!userDoc) {
+    //     return {
+    //         notFound: true;
+    //     }
+    // }
+
+    return {
+        props: { user, posts }, //will be passed to the page component as props
+    };
+}
+
 export default function UserProfilePage({user, posts}){
     return(
         <main>
