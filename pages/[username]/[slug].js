a5b7aaec24600bfc923972f99900bbbd302f629e
Fabio Rodriguez <alessf13@gmail.com>
2022-03-18 15:34:27 +0000

    getStaticProps for slug.js

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,5 +1,33 @@
 // import styles from '../../styles/Post.module.css';
+import PostContent from "../../Components/PostContent"
+import { UserContext } from "../../lib/context"
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
+import { doc, getDocs, getDoc, collectionGroup, query, limit, getFirestore } from 'firebase/firestore';
 
+import Link from "next/link";
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useContext } from 'react';
+
+export async function getStaticProps({ params }) {
+    const { username, slug} = params;
+    const userDoc = await getUserWithUsername(username);
+
+    let post;
+    let path;
+
+    if (userDoc) {
+        const postRef = doc(getFirestore(), userDoc.ref.path, 'posts', slug);
+
+        post = postToJSON(await getDoc(postRef))
+
+        path = postRef.path;
+    }
+
+    return {
+        props: {post, path},
+        revalidate: 100,
+    }
+}
 
 
 export default function Post({}) {
